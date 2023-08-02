71e8b6bc8dd73babdfc7fafaec09df0887d6545e
Marvin Borucki <85772214+MarvMarv44@users.noreply.github.com>
2023-08-02 14:23:39 +0200

    java classes basic done

--- backend/src/main/java/de/neuefische/backend/ActivityRepository.java
+++ backend/src/main/java/de/neuefische/backend/ActivityRepository.java
@@ -1,7 +1,13 @@
 package de.neuefische.backend;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class ActivityRepository {
+    private final List<Activity> activities = new ArrayList<>();
 }
--- backend/src/main/java/de/neuefische/backend/ActivityService.java
+++ backend/src/main/java/de/neuefische/backend/ActivityService.java
@@ -1,8 +1,10 @@
 package de.neuefische.backend;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ActivityService {
-
+    private final ActivityRepository activityRepository = new ActivityRepository();
 }
