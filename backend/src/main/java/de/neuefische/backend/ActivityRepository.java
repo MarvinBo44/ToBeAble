8bd71ca93ea4cf5d5c68ba63dc5bc1cb33b3aeac
mrg <rgmengedoth@gmail.com>
2023-08-02 12:18:31 +0200

    add java classes

---
+++ backend/src/main/java/de/neuefische/backend/ActivityRepository.java
@@ -0,0 +1,7 @@
+package de.neuefische.backend;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ActivityRepository {
+}
---
+++ backend/src/main/java/de/neuefische/backend/ActivityService.java
@@ -0,0 +1,8 @@
+package de.neuefische.backend;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ActivityService {
+
+}
