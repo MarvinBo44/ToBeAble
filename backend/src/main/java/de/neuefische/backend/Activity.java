20d7973855d833e322c6cbc209d70de6756c9df9
mrg <rgmengedoth@gmail.com>
2023-08-09 15:45:20 +0200

    add: MongoDB and setting up the backend

--- backend/src/main/java/de/neuefische/backend/Activity.java
+++ backend/src/main/java/de/neuefische/backend/Activity.java
@@ -2,9 +2,11 @@ package de.neuefische.backend;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Activity {
     private String id;
     private String activityName;
